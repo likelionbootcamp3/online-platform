81e62f3768e708107475dba06515fa838fb76167
zizpzn <zizpzn@gmail.com>
2023-01-17 14:57:44 +0700

    feature: update span

--- components/ui/Header/index.tsx
+++ components/ui/Header/index.tsx
@@ -18,7 +18,7 @@ const Header = () => {
               className="inline-flex items-center justify-center rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
               type="button"
             >
-              <span className="text-sm font-medium"> View Website </span>
+              <span className="text-sm font-medium"> Haha </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="ml-1.5 h-4 w-4"
